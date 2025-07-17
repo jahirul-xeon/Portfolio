fddc9aec2a65b50ba0e3134f425dd631215c85ae
Jahirul Islam Jihan <xeon.jahirul@gmail.com>
2025-07-17 13:15:56 +0600

    ignore rules

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,7 @@ const nextConfig: NextConfig = {
   },
   // This is required to support PostHog trailing slash API requests
   skipTrailingSlashRedirect: true,
+  ignoreDuringBuilds: true,
 };
 
 export default nextConfig;
